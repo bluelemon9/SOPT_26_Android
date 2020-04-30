ad09b97a8674bbb3d35fcde25df16e401311a1fb
bluelemon9 <ghlwls111@gmail.com>
2020-04-30 17:53:41 +0900

    과제1

---
+++ Seminar1/SOPT_Seminar1_HW1/app/src/main/java/com/example/sopt_seminar1_hw1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sopt_seminar1_hw1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
